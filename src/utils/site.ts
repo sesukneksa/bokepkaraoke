ef2ce731276019d16c2f226f8184b22c7f8dcc12
sesukneksa <sesukneksa@gmail.com>
2025-07-15 01:05:50 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Bokep Karaoke';
-export const desk = 'Video Bokep Indo Jepang Jav Hijab Tante Viral Terbaru Bocil Ngentot Jilbab Smp Skandal';
-export const url = 'https://bokepkaraoke.pages.dev';
-export const terbit = '2025-06-27T00:01:03Z';
